baeec703b0182bccbca63489ac188a566e0dedf3
rafal <rafal_walasek@interia.pl>
2024-12-07 00:06:24 +0100

    dodanie klasy menu i obsługi wyjatku oraz interakcji z uzytkownikiem

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println("Football Manager Started!");
+
+        Scanner scanner = new Scanner(System.in);
+        Menu menu = new Menu();
+
+        boolean isProgramRuuning = true;
+        while (isProgramRuuning) {
+
+            menu.menu();
+            System.out.print("Wybierz pozycję z menu: ");
+
+            try {
+                switch (scanner.nextInt()) {
+                    case 1 -> System.out.println("wybór 1");
+                    case 2 -> System.out.println("wybór 2");
+                    case 3 -> isProgramRuuning = false;
+                    default -> System.out.println("Błąd: podaj cyfrę z MENU!");
+                }
+            } catch (InputMismatchException e) {
+                scanner.nextLine(); //czyszczenie bufora
+                System.out.println("Niepoprawne dane! Spróbuj jeszcze raz!");
+            }
+
+        }
+
+        scanner.close();
+        System.out.println("Football Manager Finish!!");
 
     }
 }
---
+++ src/Menu.java
@@ -0,0 +1,9 @@
+public class Menu {
+    void menu() {
+        System.out.println("----------  MENU  -----------");
+        System.out.println("|   1 -> Dodaj zawodnika    |");
+        System.out.println("|   2 -> Lista zawodników   |");
+        System.out.println("|   3 -> Wyjście            |");
+        System.out.println("-----------------------------");
+    }
+}
